36e8ea5bf356f351836f3249bf8c163c90730740
Guilherme Rodrigues <guiihpr@gmail.com>
2016-04-28 12:26:17 -0300

    Alteracao no endereco do socket io

--- www/js/controllers/GlobalCtrl.js
+++ www/js/controllers/GlobalCtrl.js
@@ -1,6 +1,7 @@
 window.app.controller('GlobalCtrl',['$http',function($http){
     //Registrar os eventos aqui....
     window.io = io.connect("http://192.168.0.149:3000");
+    
 
     io.on('pergunta',function(data){
         alert(data);
